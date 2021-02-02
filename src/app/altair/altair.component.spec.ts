b05042bbb15ba8c71ab1444bf9c8763c67a4d28e
Paul Scott <8pscott@gmail.com>
2021-02-01 19:16:32 -0500

    eslint

--- src/app/altair/altair.component.spec.ts
+++ src/app/altair/altair.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AltairComponent } from './altair.component';
 
@@ -6,7 +6,7 @@ describe('AltairComponent', () => {
   let component: AltairComponent;
   let fixture: ComponentFixture<AltairComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AltairComponent ]
     })
